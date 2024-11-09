80fe69aacc94d874f21ed77efc691a8643383c48
thirdygayares <gayaresthird@gmail.com>
2024-11-09 11:43:20 +0800

    Initial commit

---
+++ app.py
@@ -0,0 +1,57 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+# TODO: Restaurant List of Dictionaries
+# image_url, restaurant_name, status, location, id
+restaurants = [
+    {
+        "id": 1,
+        "location": "Makati",
+        "restaurant_name": "Restaurant One",
+        "status": "Closed",
+        "image_url": "https://images.pexels.com/photos/67468/pexels-photo-67468.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+    },
+    {
+        "id": 2,
+        "location": "Pasig",
+        "restaurant_name": "Restaurant Two",
+        "status": "Open",
+        "image_url": "https://images.pexels.com/photos/25858122/pexels-photo-25858122/free-photo-of-patio-of-cafe.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+    },{
+        "id": 3,
+        "location": "Mandaluyong",
+        "restaurant_name": "Restaurant Three",
+        "status": "Open",
+        "image_url": "https://images.pexels.com/photos/16550341/pexels-photo-16550341/free-photo-of-facade-of-urban-building.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+    },{
+        "id": 4,
+        "location": "Quezon CIty",
+        "restaurant_name": "Restaurant Four",
+        "status": "Open",
+        "image_url": "https://images.pexels.com/photos/12935080/pexels-photo-12935080.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+    },{
+        "id": 5,
+        "location": "Alabang",
+        "restaurant_name": "Restaurant Five",
+        "status": "Closed",
+        "image_url": "https://images.pexels.com/photos/8921562/pexels-photo-8921562.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+    },{
+        "id": 6,
+        "location": "Pasig",
+        "restaurant_name": "Restaurant Six",
+        "status": "Closed",
+        "image_url": "https://images.pexels.com/photos/14646764/pexels-photo-14646764.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+    },
+]
+
+
+
+@app.route('/')
+def hello_world():
+    print(restaurants);
+    return render_template('index.html', restaurants=restaurants)
+
+if __name__ == '__main__':
+    app.run(debug=True)
